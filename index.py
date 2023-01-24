0b60c282485f7ee4b96d27273c6111b2dcd2fa49
vascoarizna <eviljia@gmail.com>
2023-01-24 09:33:46 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,29 @@
+
 from flask import Flask
-
+from flask import request
 app = Flask(__name__)
+@app.route("/")
+def index():
+    celsius = request.args.get("celsius", "")
+    if celsius:
+        fahrenheit = fahrenheit_from(celsius)
+    else:
+        fahrenheit = ""
+    return (
+        """<form action="" method="get">
+                Celsius temperature: <input type="text" name="celsius">
+                <input type="submit" value="Convert to Fahrenheit">
+            </form>"""
+        + "Fahrenheit: "
+        + fahrenheit
+    )
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+
+    except ValueError:
+        return "invalid input"
