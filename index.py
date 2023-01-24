c2e4abc024f79a24436ae3c45a33b3581fd3e801
vascoarizna <eviljia@gmail.com>
2023-01-24 13:10:33 +0100

    Update index.py

--- index.py
+++ index.py
@@ -29,42 +29,15 @@ def index():
         + fahrenheit
     )
     
-def get_df(final_matrix,threshold):
-    look_for_value=final_matrix.iloc[1:,2:61]
-    my_df_values=look_for_value[look_for_value<=threshold]
-    range_balance_df=final_matrix[['all_balance_x','all_balance_y']].rename(columns={'all_balance_x':'the_min','all_balance_y':'the_max'})
-    my_df_values=my_df_values.merge(range_balance_df,left_index=True,right_index=True)
-    return my_df_values
 
-def get_bonus(dep,ratio):
-    bonus_to_give = (dep*ratio)/(30-ratio)
-    final_balance=dep+bonus_to_give
-    return final_balance,bonus_to_give
 
-def get_balance_range(final_matrix,threshold,balance):
-    my_df_values = get_df(final_matrix,threshold) 
-    #the_row=my_df_values[(my_df_values['the_min']>=balance)].iloc[0,:].name
-    the_row=my_df_values[(my_df_values['the_min']<=balance)&(my_df_values['the_max']>=balance)].iloc[0,:].name
-    return the_row
-
-def validate_percentage(final_matrix,balance,ratio,threshold):
-    my_df_values = get_df(final_matrix,threshold)
-    the_index=get_balance_range(final_matrix,threshold,balance)
+def validate_percentage(the_row,final_matrix,my_df_values,balance,ratio,threshold):
+    the_index=the_row
     if my_df_values.loc[the_index,ratio]<=threshold:
         return 'True'
     else:
         return 'False' 
 
-def look_in_DF(final_matrix,df,threshold,deposit):
-    the_list=[]
-    df=df[df['the_min']>deposit]
-    for i in df.index:
-        for j in df.columns:
-            if df.loc[i,j]<threshold:
-                balance,bonus=get_bonus(deposit,float(j))
-                validate_percentage(final_matrix,balance,j,threshold)
-                the_list.append([deposit,get_balance_range(final_matrix,threshold,balance),balance,bonus,float(j),validate_percentage(final_matrix,balance,j,threshold)])
-    return the_list
 
 def combinations(threshold,deposit):
     final_matrix=pd.read_csv("https://www.dropbox.com/s/g21186mjf80hy0n/final_matrix_parquet.csv?dl=1")
@@ -78,13 +51,15 @@ def combinations(threshold,deposit):
     
     #the_list=look_in_DF(final_matrix,my_df_values,threshold,deposit)
     the_list=[]
-    df=my_df_values[my_df_values['the_min']>deposit]
+    my_df_values=my_df_values[my_df_values['the_min']>deposit]
     for i in my_df_values.index:
         for j in my_df_values.columns:
             if my_df_values.loc[i,j]<threshold:
-                balance,bonus=get_bonus(deposit,float(j))
-                validate_percentage(final_matrix,balance,j,threshold)
-                the_list.append([deposit,get_balance_range(final_matrix,threshold,balance),balance,bonus,float(j),validate_percentage(final_matrix,balance,j,threshold)])
+               # balance,bonus=get_bonus(deposit,float(j))
+                bonus = (deposit*float(j))/(30-float(j))
+                balance=deposit+bonus
+                the_row=my_df_values[(my_df_values['the_min']<=balance)&(my_df_values['the_max']>=balance)].iloc[0,:].name
+                the_list.append([deposit,the_row,balance,bonus,float(j),validate_percentage(the_row,final_matrix,my_df_values,balance,j,threshold)])
     
     
     final_df=pd.DataFrame(the_list)
@@ -94,3 +69,6 @@ def combinations(threshold,deposit):
     
     #return to_produce
     return render_template('table.html', tables=[to_produce.to_html()], titles=[''])
+
+
+
