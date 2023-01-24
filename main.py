58070afcaa10832eb6dc14944fdcbc052e9c3031
vascoarizna <eviljia@gmail.com>
2023-01-24 09:27:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -68,5 +68,5 @@ def fahrenheit_from(celsius):
         return "invalid input"
 
 if __name__ == "__main__":
-
-    app.run(host="127.0.0.1", port=8080, debug=True)
+#    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run()
